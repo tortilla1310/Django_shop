255c4caa1488d5baa3b0318ecd2e9e0d761c55b0
tortilla <tortilla1310@gmail.com>
2022-01-23 21:56:33 +0100

    changes
    smth

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,11 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'mainapp/index.html')
-    
+    return render(request, 'mainapp/index.html', context={
+        'variable': 'Hello!'
+    })
+
+
 def products(request):
     return render(request, 'mainapp/products.html')
+
 
 def contact(request):
     return render(request, 'mainapp/contact.html')

e7b6b429cc54bf977412cfb05df19d6e8e2ac155
tortilla <tortilla1310@gmail.com>
2022-01-24 22:03:31 +0100

    New homework 2 is done

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,18 +1,39 @@
-from multiprocessing import context
+import json
+from math import prod
+from django.http.request import HttpRequest
+import imp
 from django.shortcuts import render
 
-# Create your views here.
+MENU_LINKS = [
+    {'url': 'main', 'name': 'домой'},
+    {'url': 'products', 'name': 'продукты'},
+    {'url': 'contact', 'name': 'контакты'},
+]
 
 
 def index(request):
     return render(request, 'mainapp/index.html', context={
-        'variable': 'Hello!'
+        'title': 'Главная',
+        'menu_links': MENU_LINKS,
+
     })
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    with open('./products.json', 'r') as file:
+        products = json.load(file)
+
+    return render(request, 'mainapp/products.html', context={
+        'title': 'Продукты',
+        'products': products,
+        'menu_links': MENU_LINKS,
+
+    })
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', context={
+        'title': 'Контакты',
+        'menu_links': MENU_LINKS,
+
+    })

48a7ba17f61c55f256462cae3e3751b1a55df01b
tortilla <tortilla1310@gmail.com>
2022-01-30 22:48:12 +0100

    homework3 is done

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='name', max_length=100)
+    description = models.TextField(verbose_name='description', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='name', max_length=100)
+    price = models.DecimalField(
+        verbose_name='price', max_digits=7, decimal_places=2, default=0)
+    color = models.PositiveIntegerField(verbose_name='color', default=0x000000)
+    description = models.TextField(verbose_name='description', blank=True)
+    image = models.ImageField(verbose_name='image',
+                              blank=True, upload_to='products')
+    quantity = models.PositiveIntegerField(verbose_name='quantity', default=0)
+
+    def __str__(self):
+        return self.name
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,32 +3,48 @@ from math import prod
 from django.http.request import HttpRequest
 import imp
 from django.shortcuts import render
+from .models import Product, ProductCategory
 
 MENU_LINKS = [
     {'url': 'main', 'name': 'домой'},
-    {'url': 'products', 'name': 'продукты'},
+    {'url': 'products:products', 'name': 'продукты'},
     {'url': 'contact', 'name': 'контакты'},
 ]
 
 
 def index(request):
-    return render(request, 'mainapp/index.html', context={
-        'title': 'Главная',
-        'menu_links': MENU_LINKS,
+    products = Product.objects.all()[:4]
 
-    })
+    return render(
+        request,
+        'mainapp/index.html',
+        context={
+            'title': 'Главная',
+            'menu_links': MENU_LINKS,
+            "products": products,
+        },
+    )
 
 
 def products(request):
+    categories = ProductCategory.objects.all()
     with open('./products.json', 'r') as file:
         products = json.load(file)
 
-    return render(request, 'mainapp/products.html', context={
-        'title': 'Продукты',
-        'products': products,
-        'menu_links': MENU_LINKS,
+    return render(
+        request,
+        'mainapp/products.html',
+        context={
+            'title': 'Продукты',
+            'products': [],
+            'menu_links': MENU_LINKS,
+            "categories": categories,
 
-    })
+        },)
+
+
+def category(request, pk):
+    return products(request)
 
 
 def contact(request):
@@ -36,4 +52,4 @@ def contact(request):
         'title': 'Контакты',
         'menu_links': MENU_LINKS,
 
-    })
+    },)
